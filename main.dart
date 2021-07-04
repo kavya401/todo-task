35bf487b9550a1987767ccfee140478485512035
kavya401 <86911573+kavya401@users.noreply.github.com>
2021-07-04 15:22:23 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+//The main function is starting point of all flutter app
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar:AppBar(
+        title:Text('My list') ,
+      ),
+    ),
+  ),
+  );
+}
